9aaa113dff1c22e9a7b1f348d3d2ef754cd978e6
i.melentsov <i.melentsov@gmail.com>
2014-01-20 17:08:38 +0600

    fix encoding

--- mistakes/src/main/java/Reader.java
+++ mistakes/src/main/java/Reader.java
@@ -14,7 +14,6 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
-
 import com.google.common.collect.TreeMultiset;
 
 public class Reader {
